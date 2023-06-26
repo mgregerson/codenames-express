f773281d7ac7a597969077c40cd9b6a515273d49
mgregerson <mgregerson4321@gmail.com>
2023-06-26 17:06:08 -0400

    Newly created API - Created server.js running on port 3000, added dependencies to package.json

---
+++ app.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/** Express app for jobly. */
+
+const express = require("express");
+const cors = require("cors");
+
+const cardsRoutes = require("./routes/cards");
+
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+
+app.use("/cards", cardsRoutes);
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+  throw new NotFoundError();
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+  if (process.env.NODE_ENV !== "test") console.error(err.stack);
+  const status = err.status || 500;
+  const message = err.message;
+
+  return res.status(status).json({
+    error: { message, status },
+  });
+});
+
+module.exports = app;
