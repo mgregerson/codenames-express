bc982c6f3ce21fe26da9b4222ec4cf0365235ef6
mgregerson <mgregerson4321@gmail.com>
2023-06-30 10:22:16 -0400

    Added socket logic - joinTeam, teamUpdate, join

--- server.js
+++ server.js
@@ -1,8 +1,64 @@
-"use strict";
-
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const app = require("./app");
 const { PORT } = require("./config");
 
-app.listen(PORT, function () {
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://localhost:3000", "http://localhost:3002"],
+  },
+});
+
+const players = {}; // Store connected players
+const redTeam = [];
+const blueTeam = [];
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  console.log(socket);
+
+  // Handle player joining
+  socket.on("join", (playerName) => {
+    players[socket.id] = playerName;
+    console.log(`${playerName} joined the game`);
+
+    // Notify all players about the new player
+    io.emit("playerJoined", playerName);
+  });
+
+  socket.on("joinTeam", (team, role, name) => {
+    if (team === "red") {
+      redTeam.push({ team, role, name, id: socket.id });
+      console.log(redTeam);
+    } else {
+      blueTeam.push({ team, role, name, id: socket.id });
+      console.log(blueTeam);
+    }
+    io.emit("teamUpdate", redTeam, blueTeam);
+  });
+
+  // Handle player making a guess
+  socket.on("makeGuess", (guess) => {
+    // Validate the guess and update game state
+
+    // Notify all players about the guess
+    io.emit("guessMade", guess);
+  });
+
+  // Handle player disconnecting
+  socket.on("disconnect", () => {
+    const playerName = players[socket.id];
+    delete players[socket.id];
+    console.log(`${playerName} left the game`);
+
+    // Notify all players about the player leaving
+    io.emit("playerLeft", playerName);
+  });
+});
+
+httpServer.listen(PORT, () => {
   console.log(`Started on http://localhost:${PORT}`);
 });
+
+module.exports = io;
