b9f3723b4947479519d203d3ffa194746d46d2e9
mgregerson <mgregerson4321@gmail.com>
2023-07-06 12:11:55 -0400

    Updated card model - Returns an array of objects as so: {word: word, team: red | blue, guess: , selected: false}

--- models/card.js
+++ models/card.js
@@ -1,11 +1,46 @@
 "use strict";
 
-const { red } = require("colors");
 const CARDS = require("../data/cards");
 
 class Card {
+  /** Assign cards to game */
+  static async assignCardsToGame() {
+    const redTeamCount = 7;
+    const blueTeamCount = 8;
+    const deathCardCount = 1;
+    const neutralCardCount = 9;
+
+    const cardValues = await this._getGameCards();
+
+    const cards = [];
+
+    for (let i = 0; i < redTeamCount; i++) {
+      const card = cardValues.pop();
+      cards.push({ word: card, team: "red", guess: "", selected: false });
+    }
+
+    for (let i = 0; i < blueTeamCount; i++) {
+      const card = cardValues.pop();
+      cards.push({ word: card, team: "blue", guess: "", selected: false });
+    }
+
+    for (let i = 0; i < neutralCardCount; i++) {
+      const card = cardValues.pop();
+      cards.push({ word: card, team: "neutral", guess: "", selected: false });
+    }
+
+    for (let i = 0; i < deathCardCount; i++) {
+      const card = cardValues.pop();
+      cards.push({ word: card, team: "death", guess: "", selected: false });
+    }
+
+    const shuffledCards = this._shuffleArray(cards);
+
+    return shuffledCards;
+  }
+
   /** Retrieve 25 randomly selected words from our list of words */
-  static async getGameCards() {
+  static async _getGameCards() {
     const cards = [];
     const items = CARDS.items;
     while (cards.length < 25) {
@@ -17,48 +52,8 @@ class Card {
     return cards;
   }
 
-  static async assignCardsToGame() {
-    const redTeamCount = 7;
-    const blueTeamCount = 8;
-    const deathCardCount = 1;
-    const neutralCardCount = 9;
-
-    const cardValues = await this.getGameCards();
-
-    const cards = {};
-
-    for (let i = 0; i < redTeamCount; i++) {
-      const card = cardValues.pop();
-      cards[card] = "red";
-    }
-
-    for (let i = 0; i < blueTeamCount; i++) {
-      const card = cardValues.pop();
-      cards[card] = "blue";
-    }
-
-    for (let i = 0; i < neutralCardCount; i++) {
-      const card = cardValues.pop();
-      cards[card] = "neutral";
-    }
-
-    for (let i = 0; i < deathCardCount; i++) {
-      const card = cardValues.pop();
-      cards[card] = "death";
-    }
-
-    const shuffledCards = this.shuffleObject(cards);
-
-    return shuffledCards;
-  }
-
-  static shuffleObject(obj) {
-    const entries = Object.entries(obj);
-    const shuffledEntries = this.shuffleArray(entries);
-    return Object.fromEntries(shuffledEntries);
-  }
-
-  static shuffleArray(array) {
+  /** Shuffle array */
+  static _shuffleArray(array) {
     // Fisher-Yates shuffle algorithm
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -67,6 +62,7 @@ class Card {
     return array;
   }
 
+  /** Get random item from array */
   static _getRandomItem(array) {
     const randomIndex = Math.floor(Math.random() * array.length);
     return array[randomIndex];
